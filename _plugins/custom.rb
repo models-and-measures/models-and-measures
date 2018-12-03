75186e96a90486c79354f89160e2b9b49c0558a7
jstoehr <44804838+jstoehr@users.noreply.github.com>
2018-12-03 13:13:01 +0100

    Update custom.rb

--- _plugins/custom.rb
+++ _plugins/custom.rb
@@ -1,13 +1,16 @@
 module Jekyll
-  class MarkdownBlock < Liquid::Block
+  class MarkdownTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
+      @text = text.strip
     end
     require "kramdown"
     def render(context)
-      content = super
-      "#{Kramdown::Document.new(content).to_html}"
+      tmpl = File.read File.join Dir.pwd, "_includes", @text
+      site = context.registers[:site]
+      tmpl = (Liquid::Template.parse tmpl).render site.site_payload
+      html = Kramdown::Document.new(tmpl).to_html
     end
   end
 end
-Liquid::Template.register_tag('markdown', Jekyll::MarkdownBlock)
+Liquid::Template.register_tag('markdown', Jekyll::MarkdownTag)
